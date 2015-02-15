7a00db5486bf6eeaecde35f0901f620558f9fa28
Nadav Cohen <nadav@benjycook.(none)>
2015-02-15 08:59:29 -0500

    kind of working

--- JSONParser-master/test/test.php
+++ JSONParser-master/test/test.php
@@ -2,50 +2,99 @@
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'package/JSONParser.php';
 
 error_reporting(E_ALL);
-
+ini_set('display_errors','On');
+ini_set('max_execution_time',5);
 $GLOBALS['currentObjSt'] = false;
 $GLOBALS['currentObjListings'] = false;
 $GLOBALS['currentObjSingleListing'] = false;
 $GLOBALS['isInListings'] = false;
 
+function print_a_global($str) {
+  echo $str.":".print_r($GLOBALS[$str],true)."<br>";
+}
+
+function nadilog() {
+  print_a_global('currentObjSt');
+  print_a_global('currentObjListings');
+  print_a_global('currentObjSingleListing');
+  print_a_global('isInListings');
+}
+
 function objStart($value, $property) {
+  echo "<hr>";
 	if(!$GLOBALS['isInListings']) $GLOBALS['currentObjSt'] = array();
+	else if($GLOBALS['isInListings']) {
+	  echo "single listings = array";
+	  $GLOBALS['currentObjSingleListing']=array();
+	}
 	else $GLOBALS['currentObjSingleListing'] = array();
+	echo "objStart()";
+	nadilog();
 }
 
 function objEnd($value, $property) {
-		
-	print_r($GLOBALS['benjy']);
-	echo "<BR>";
+		echo "<hr>";
+		if(!$GLOBALS['isInListings']){
+		  nadilog();
+		  
+		}
+		echo "objEnd()";
+    
 }
 
 function arrayStart($value, $property) {
-	$GLOBALS['benjy'] = array();
-	//printf("[<br>");
+	echo "<hr>";
+	$GLOBALS['currentObjListings']=array();
+	echo "arrayStart()";
+  nadilog();
 }
 
 function arrayEnd($value, $property) {
 	//printf("]<br>");
-	print_r($GLOBALS['benjy']);
-	echo "<BR>";
+	echo "<hr>";
+	if($GLOBALS['isInListings']){
+	  $GLOBALS['isInListings']=false;
+	  var_dump($GLOBALS['currentObjSingleListing']);
+	  
+	}
+	echo "arrayEnd()";
+	
+  nadilog();
 }
 
-function value($value, $property) {
-	
-	if($GLOBALS['currentObjSt']&&(!$GLOBALS['isInListings'])) $GLOBALS['currentObjSt'][$property]=$value;
-	else if($GLOBALS['currentObjSingleListing']) $GLOBALS['currentObjSingleListing'][$property] = $value;
+function value($property,$value) {
+  echo "<hr>";
+	if($value=="listings") {
+	  echo "property = listings";
+	  $GLOBALS['isInListings'] =true;
+    
+	}
+	$a = is_array($GLOBALS['currentObjSt']);
+	$b = !$GLOBALS['isInListings'];
+
+	if($a&&$b) {
+    echo "its working";
+	  $GLOBALS['currentObjSt'][$property]=$value;
+	}
+	else if(is_array($GLOBALS['currentObjSingleListing'])) $GLOBALS['currentObjSingleListing'][$property] = $value;
+	  echo "value() ".$property." => ".$value."<br>";
+    nadilog();
 
 	//$GLOBALS['benjy'][$property] = $value;
 }
 
-function property($value, $property) {
+function property($property,$value) {
 	//printf("Property: %s\n", $value);
-	value($value, $property);
-}
+	//value($value, $property);
+	if($property=="listings") {
+	  $GLOBALS['isInListings']=true;
+	}
+	//echo $property.":".$value."   YOOOOO!!!####";
+ }
 
 function scalar($value, $property) {
 	//printf("Value: %s\n", $value);
-	value($value, $property);
+	value($property, $value);
 }
 
 // initialise the parser object
